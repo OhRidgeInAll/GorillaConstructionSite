b36b643ee2622cb259e91786b662f660cbe3122b
OhRidgeInAll <8bitOriginal@gmail.com>
2025-04-24 14:52:33 -0400

    Organized Scenes and Player Anims
    Organization pass

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -3,4 +3,4 @@ extends Control
 
 func _on_button_pressed():
 	get_tree().paused = false
-	get_tree().change_scene_to_file("res://level1.tscn")
+	get_tree().change_scene_to_file("res://scenes/level1.tscn")
